a37e9472ae1ae16bda1c1eb7c662f3f649ea6f0e
raphael42 <raphael.bellon42@gmail.com>
2024-01-21 22:13:47 +0100

    :sparkles: Switch from annotations to attribute and add 2 fields in Game entity

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -2,34 +2,31 @@
 
 namespace App\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
- */
+#[ORM\Entity(repositoryClass: "App\Repository\GameRepository")]
 class Game
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=200)
-     */
+    #[ORM\Column(type: "string", length: 200)]
     private $url;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Column(type: "integer")]
     private $increment;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
+    #[ORM\Column(type: "string", length: 255)]
     private $fen;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $time = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_insert = null;
 
     public function getId(): ?int
     {
@@ -68,6 +65,30 @@ class Game
     public function setFen(string $fen): self
     {
         $this->fen = $fen;
+
+        return $this;
+    }
+
+    public function getTime(): ?float
+    {
+        return $this->time;
+    }
+
+    public function setTime(?float $time): static
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDateInsert(): ?\DateTimeInterface
+    {
+        return $this->date_insert;
+    }
+
+    public function setDateInsert(\DateTimeInterface $date_insert): static
+    {
+        $this->date_insert = $date_insert;
 
         return $this;
     }
--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -6,37 +6,25 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\PlayerRepository")
- */
+#[ORM\Entity(repositoryClass: "App\Repository\PlayerRepository")]
 class Player
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=20)
-     */
+    #[ORM\Column(type: "string", length: 20)]
     private $color;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\game")
-     * @ORM\JoinColumn(nullable=false)
-     */
+    #[ORM\ManyToOne(targetEntity: "App\Entity\Game")]
+    #[ORM\JoinColumn(nullable: false)]
     private $game;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
+    #[ORM\Column(type: "boolean")]
     private $game_creator;
 
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
+    #[ORM\Column(type: "integer", nullable: true)]
     private $time_left;
 
     public function getId(): ?int
@@ -88,6 +76,23 @@ class Player
     public function setTimeLeft(?int $time_left): self
     {
         $this->time_left = $time_left;
+
+        return $this;
+    }
+
+    public function isGameCreator(): ?bool
+    {
+        return $this->game_creator;
+    }
+
+    public function getGame(): ?game
+    {
+        return $this->game;
+    }
+
+    public function setGame(?game $game): static
+    {
+        $this->game = $game;
 
         return $this;
     }
