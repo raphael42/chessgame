e622a9a259723649beb35772a1cfa15f261cebfb
raphael42 <raphael.bellon42@gmail.com>
2024-02-03 18:39:41 +0100

    :sparkles: Create webpack app for all the site with scss and bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    .addEntry('allSite', './assets/allSite.js')
     .addEntry('gameJs', './assets/ChessMoves.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
@@ -54,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
