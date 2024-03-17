6e342a75385eb9c74e4bb7ddda82442052a29617
raphael42 <raphael.bellon42@gmail.com>
2024-03-17 19:56:55 +0100

    :sparkles: Add mailer DSN, wait for informations about debian system mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -12,7 +14,7 @@ use App\Form\Contact;
 
 class ContactController extends AbstractController
 {
-    public function contactfunction(Request $request, EntityManagerInterface $entityManager): Response
+    public function contactfunction(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
         $formContact = $this->createForm(Contact::class);
         $formContact->handleRequest($request);
@@ -20,6 +22,15 @@ class ContactController extends AbstractController
             $data = $formContact->getData();
 
             // TODO : Send the email
+            $email = (new Email())
+            ->from('contact@freechess.fr')
+            ->to('raphael.bellon42@gmail.com')
+            ->subject('Symfony mailer!')
+            ->text('Text integration')
+            ->html('<p>Html integration</p>');
+
+            $result = $mailer->send($email);
+            dump($result);die;
         }
 
         return $this->render('contact.html.twig', [
