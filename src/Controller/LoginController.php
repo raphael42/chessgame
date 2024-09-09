b98a2229d3893337f94dd4dc12724970d9ffe75e
raphael42 <raphael.bellon42@gmail.com>
2024-09-09 19:48:40 +0200

    :sparkles: Add symfony regirtration system

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-use App\Form\Contact;
+use App\Form\Login;
 use App\Entity;
 
-class UserController extends AbstractController
+class LoginController extends AbstractController
 {
     public function loginfunction(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -21,15 +21,8 @@ class UserController extends AbstractController
             // TODO
         }
 
-        $formRegister = $this->createForm(Register::class);
-        $formRegister->handleRequest($request);
-        if ($formRegister->isSubmitted() && $formRegister->isValid()) {
-            // TODO
-        }
-
         return $this->render('login.html.twig', [
             'formLogin' => $formLogin->createView(),
-            'formRegister' => $formRegister->createView(),
             'status' => $_GET['status'] ?? null,
         ]);
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -31,6 +33,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Player::class)]
     private Collection $players;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -133,6 +138,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $player->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
