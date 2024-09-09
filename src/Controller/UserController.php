ff7f51125ede99b157087b5ff780b61d6b1abbe9
raphael42 <raphael.bellon42@gmail.com>
2024-09-09 19:09:41 +0200

    :construction: Keep building user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\Contact;
+use App\Entity;
 
 class UserController extends AbstractController
 {
-    public function loginfunction(): Response
+    public function loginfunction(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('login.html.twig', [
+        $formLogin = $this->createForm(Login::class);
+        $formLogin->handleRequest($request);
+        if ($formLogin->isSubmitted() && $formLogin->isValid()) {
+            // TODO
+        }
 
+        $formRegister = $this->createForm(Register::class);
+        $formRegister->handleRequest($request);
+        if ($formRegister->isSubmitted() && $formRegister->isValid()) {
+            // TODO
+        }
+
+        return $this->render('login.html.twig', [
+            'formLogin' => $formLogin->createView(),
+            'formRegister' => $formRegister->createView(),
+            'status' => $_GET['status'] ?? null,
         ]);
     }
 }
---
+++ src/Form/Login.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class Login extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Votre prénom',
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ])
+        ;
+    }
+}
